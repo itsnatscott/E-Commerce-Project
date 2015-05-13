1264417d1879b9a24dc90ce57969f8775be1cc5b
Your Name <you@example.com>
2015-05-13 15:01:40 -0400

    put post routes

--- server.rb
+++ server.rb
@@ -34,7 +34,7 @@ get '/admin' do
 	# binding.pry
 	erb :admin, locals: {tshirts: tshirts, purchases: purchases}
 end
-
+#show a shirt and all of its transactions ... 'Admin show page'
 get '/admin/:id' do 
 	shirt = Tshirt.find(params[:id])
 	shirtTransactions = shirt.transactions
@@ -42,6 +42,34 @@ get '/admin/:id' do
 end
 
 
-
+#update a tshirt from 'admin show page'
+put '/admin/:id' do
+	shirt = Tshirt.find(params[:id])
+	new_pic = params[:shirt_url]
+	new_price = params[:price]
+	new_quant = params[:quant]
+	shirt.update({quantity: new_quant, price: new_price, img_url: new_pic})
+	redirect ('/admin')
+end
+#subtract purchased quantity from shirt id, add transaction row to table
+post 'index/:id' do
+	shirt = Tshirt.find(params[:id])
+	shirt_quant = shirt.quantity
+	trans_quant= params[:quant].to_i
+	new_quant = shirt_quant - trans_quant
+	trans_email= params[:email]
+	shirt_id= params[:id]
+	shirt.update({quantity: new_quant})
+	Transaction.create({email: trans_email, tshirt_id: shirt_id})
+	redirect ('index/#{params[:id]}/confirm')
+end
+#add a new shirt to the site
+post '/admin' do
+	new_pic = params[:shirt_url]
+	new_price = params[:price]
+	new_quant = params[:quant]
+	Tshirt.create({quantity: new_quant, price: new_price, img_url: new_pic})
+	redirect ('/admin')
+end
 
 
