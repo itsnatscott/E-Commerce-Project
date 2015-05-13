42f5b259e9b62cc5b1ee0143915dea61876f0852
Meir <meir@Meirs-Mac.local>
2015-05-13 14:24:38 -0400

    server get routes

--- server.rb
+++ server.rb
@@ -4,6 +4,7 @@ require 'sqlite3'
 
 require_relative 'models/tshirt'
 require_relative 'models/transaction'
+require_relative 'library/connection'
 
 # Home redirect
 get '/' do 
@@ -16,5 +17,31 @@ get '/index' do
 	tshirts = Tshirt.all
 	erb :index, locals: {tshirts: tshirts}
 end
+
+# order screen where user puts their data after clicking purchase button for tshirt
+get '/index/:id' do
+	params = params[:id]
+	tshirtId = Tshirt.find_by(params) 
+	erb :purchase, locals: {tshirtId: tshirtId}
+end
+
+# admin page for creation, deletion and editing of inventory
+get '/admin' do
+	# shirt = Tshirt.find(params[:id])
+	# shirtTransactions = shirt.transactions
+	purchases = Transaction.all
+	tshirts = Tshirt.all
+	# binding.pry
+	erb :admin, locals: {tshirts: tshirts, purchases: purchases}
+end
+
+get '/admin/:id' do 
+	shirt = Tshirt.find(params[:id])
+	shirtTransactions = shirt.transactions
+	erb :show, locals: {shirt: shirt, shirtTransactions: shirtTransactionss}
+end
+
+
+
 
 
